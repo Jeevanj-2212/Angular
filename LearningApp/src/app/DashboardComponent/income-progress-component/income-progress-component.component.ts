08092b163cce36f4a0f78d65dbb7985b1ebb020f
jeevan-j_tavant <jeevan.j@tavant.com>
2025-09-12 20:16:45 +0530

    name changes

---
+++ LearningApp/src/app/DashboardComponent/income-progress-component/income-progress-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Income-progress-component',
+  templateUrl: './income-progress-component.component.html',
+  styleUrls: ['./income-progress-component.component.scss']
+})
+export class IncomeProgressComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ LearningApp/src/app/IncomeTrackerComponent/income-form-component/income-form-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Income-form-component',
+  templateUrl: './income-form-component.component.html',
+  styleUrls: ['./income-form-component.component.scss']
+})
+export class IncomeFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
