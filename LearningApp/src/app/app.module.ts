8e7756e461a76d09a8f862e6c309d927ebc18e6c
jeevan-j_tavant <jeevan.j@tavant.com>
2025-09-12 19:55:33 +0530

    updated NavBar with material ui

--- LearningApp/src/app/app.module.ts
+++ LearningApp/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { SettingsComponent } from './settings-component/settings-component.compo
 import { PageNotFoundComponent } from './page-not-found-component/page-not-found-component.component';
 import { HomePageComponent } from './DashboardComponent/home-page-component/home-page-component.component';
 import { SharedModule } from "src/app/shared/shared.module";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import { SharedModule } from "src/app/shared/shared.module";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    BrowserAnimationsModule
 ],
   providers: [],
   bootstrap: [AppComponent]
--- LearningApp/src/app/shared/shared.module.ts
+++ LearningApp/src/app/shared/shared.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar-component/navbar-component.component';
+import { AppRoutingModule } from "src/app/app-routing.module";
+import { MatToolbarModule } from '@angular/material/toolbar'; 
+import { MatButtonModule } from '@angular/material/button';
 
 
 
 @NgModule({
   declarations: [NavbarComponent],
   imports: [
-    CommonModule
-  ],
+    CommonModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule
+],
   exports:[NavbarComponent]
 })
 export class SharedModule { }
